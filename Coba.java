cd18156a7087ead3d2564ab4e0f5818f05961575
GalangSefin <158027366+GalangSefin@users.noreply.github.com>
2024-01-30 20:35:45 +0700

    Update Coba.java

--- Coba.java
+++ Coba.java
@@ -1,13 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tes;
+package coba;
 
 /**
  *
  * @author MyBook Z Series
  */
 public class Coba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
